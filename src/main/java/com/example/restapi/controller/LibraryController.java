6e7bc381c9e914b80da46baa16d341e744923fd6
bitrate256 <bitrate256@gmail.com>
2022-04-03 22:14:52 +0900

    주석 정정

--- src/main/java/com/example/restapi/controller/LibraryController.java
+++ src/main/java/com/example/restapi/controller/LibraryController.java
@@ -22,9 +22,9 @@ public class LibraryController {
     private final LibraryService libraryService;
 
     /* 테스트 순서
-    1. 저자 등록/리스트조회/저자Id조회/삭제
-    2. 도서 등록/리스트조회/도서Id조회/도서isbn조회/수정/삭제
-    3. 회원 등록/리스트조회/회원Id조회/수정/삭제
+    1. 저자 등록/리스트조회/1명조회(Id)/삭제(Id)
+    2. 도서 등록/리스트조회/1권조회(Id)/1권조회(Id+Isbn)/수정(Id)/삭제(Id)
+    3. 회원 등록/리스트조회/1명조회(Id)/수정(Id)/삭제(Id)
     FINAL 도서 대출/대출연장/반납
      */
     // 1-1 저자 등록
@@ -39,9 +39,9 @@ public class LibraryController {
         return null;
     }
 
-    // 1-3 저자 Id조회
+    // 1-3 저자 1명 조회 (Id)
 
-    // 1-4 저자 삭제
+    // 1-4 저자 삭제 (Id)
 
     // 2-1 도서 등록
     @PostMapping("/book/add")
@@ -58,17 +58,17 @@ public class LibraryController {
         return ResponseEntity.ok(libraryService.readBook(isbn));
     }
 
-    // 2-3 도서 Id조회
+    // 2-3 도서 1권 조회 (Id)
     @PostMapping("/book/{bookId}")
     public ResponseEntity<Book> readBook (@PathVariable Long bookId) {
         return ResponseEntity.ok(libraryService.readBook(bookId));
     }
 
-    // 2-4 도서 isbn조회
+    // 2-4 도서 1권 조회 (Id+Isbn)
 
-    // 2-5 도서 수정
+    // 2-5 도서 수정 (Id)
 
-    // 2-6 도서 삭제
+    // 2-6 도서 삭제 (Id)
     @PostMapping("/book/delete/{bookId}")
     public ResponseEntity<Void> deleteBook (@PathVariable Long bookId) {
         libraryService.deleteBook(bookId);
@@ -83,15 +83,15 @@ public class LibraryController {
 
     // 3-2 회원 리스트 조회
 
-    // 3-3 회원 Id조회
+    // 3-3 회원 1명 조회 (Id)
 
-    // 3-4 회원 수정
+    // 3-4 회원 수정 (Id)
     @PostMapping("/member/edit/{memberId}")
     public ResponseEntity<Member> updateMember (@RequestBody MemberCreationRequest request, @PathVariable Long memberId) {
         return ResponseEntity.ok(libraryService.updateMember(memberId, request));
     }
 
-    // 3-5 회원 삭제
+    // 3-5 회원 삭제 (Id)
 
     // FINAL-1 도서 대출
     @PostMapping("/book/lend")
